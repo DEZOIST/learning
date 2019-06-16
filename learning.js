dda2d2360c631aef4796954578c29266db866a77
EG01STAL <43541396+EG01STAL@users.noreply.github.com>
2019-06-16 16:48:43 +0800

    2019-6-16-16-45

---
+++ learning.js
@@ -0,0 +1,54 @@
+        function mj1()
+        {
+            document.getElementById("demo1").innerHTML = "Hello JavaScript!";
+        }
+
+        function mj2()
+        {
+            document.getElementById('myImage').src='i/eg_bulbon.gif';
+        }
+
+        function mj3()
+        {
+            document.getElementById('myImage').src='i/eg_bulboff.gif';
+        }
+
+        function mj4()
+        {
+            document.getElementById('demo2').style.fontSize='35px';
+        }
+
+        function mj5()
+        {
+            document.getElementById('demo3').style.display='none';
+        }
+
+        function mj6()
+        {
+            document.getElementById('demo3').style.display='block';
+        }
+
+        function mj7()
+        {
+            window.alert(5 + 6);
+        }
+
+        // function mj8()
+        // {
+        //     document.write(5 + 6);
+        // }
+
+        function mj9()
+        {
+            document.getElementById("demo4").innerHTML = 5 + 6;
+        }
+
+        // function mj10()
+        // {
+        //     console.log(5 + 6);
+        // }
+
+        function mj ()
+        {
+            
+        }
